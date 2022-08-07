95a673c8e67c4fbfb44a2c2ee4bd6a8940054eac
email <or_lev@mail.ru>
2022-08-07 21:47:52 +0300

    Задача 2

--- problem_2/Program.cs
+++ problem_2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите два целых числа");
+string a = Console.ReadLine();
+string b = Console.ReadLine();
+int aa = int.Parse(a);
+int bb = int.Parse(b);
+if (aa == bb) 
+Console.WriteLine("Числа равны");
+else
+{
+    if (aa > bb)
+    {Console.Write("Max=");
+    Console.WriteLine(aa);}
+    else {
+    Console.Write("Max=");
+    Console.WriteLine(bb);
+    }
+}
