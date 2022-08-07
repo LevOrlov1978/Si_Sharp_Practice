0d4a41a1deb607a235183317a138621a8a3210ab
email <or_lev@mail.ru>
2022-08-07 23:12:05 +0300

    Задача 6

---
+++ problem_6/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите целое число");
+string a = Console.ReadLine();
+int aa = int.Parse(a);
+if ((aa %2) == 0) Console.WriteLine("Четное");
+else Console.WriteLine("Нечетное");
