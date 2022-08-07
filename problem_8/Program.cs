adebd459653c8b11520fa36392be2272d843ef75
email <or_lev@mail.ru>
2022-08-07 23:33:52 +0300

    задача 8

---
+++ problem_8/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите целое положительное число");
+string a = Console.ReadLine();
+int aa = int.Parse(a);
+int count = 1;
+while (count<aa+1)
+{if ((count %2) == 0) Console.WriteLine(count);
+count=count+1;}
