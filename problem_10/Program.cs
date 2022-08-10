741f116986e58cbd96d4fbc1127bf24870471ff1
email <or_lev@mail.ru>
2022-08-11 02:10:02 +0300

    задача 10

---
+++ problem_10/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Введите трехзначное число");
+string threeDigits = Console.ReadLine();
+int threeDigitNumber = int.Parse(threeDigits);
+int twoDigits = threeDigitNumber/10;
+Console.WriteLine($"второй знак: {twoDigits %10}");
