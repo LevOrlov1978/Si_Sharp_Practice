cfae166e4ef40460af6a8d3fbcd43cfb353a159a
email <or_lev@mail.ru>
2022-08-11 19:27:29 +0300

    задача 15

---
+++ problem_15/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите номер дня недели от 1 до 7");
+string weekDay = Console.ReadLine();
+int weekDayNumber = int.Parse(weekDay);
+if (weekDayNumber == 6 | weekDayNumber == 7)
+Console.WriteLine("выходной");
+else
+Console.WriteLine("будний");
