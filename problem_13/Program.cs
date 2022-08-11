7a8808012ad06cb84aba3b837723408197e3a20e
email <or_lev@mail.ru>
2022-08-11 19:06:30 +0300

    задача 10

---
+++ problem_13/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите целое положительное число");
+string number = Console.ReadLine();
+char[] array = number.ToCharArray();
+if (array.Length < 3)
+Console.WriteLine("третьей цифры нет");
+else
+Console.WriteLine(array[2]);
