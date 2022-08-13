067520a981a38de5b3421485952f191e6b27eb87
email <or_lev@mail.ru>
2022-08-13 19:38:10 +0300

    задача 23

---
+++ problem_23/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите N");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine("Таблица кубов от 1 до N:");
+for (int i = 1; i <= N; i++)
+{
+ Console.WriteLine(Math.Pow(i,3));  
+}
