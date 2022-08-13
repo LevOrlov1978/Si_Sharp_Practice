b573e7a5abe6886e0f6bb62259cf52924fb32070
email <or_lev@mail.ru>
2022-08-13 22:19:39 +0300

    задача 19 исправлено

--- problem_19/Program.cs
+++ problem_19/Program.cs
@@ -1,7 +1,7 @@
 ﻿Console.WriteLine("Введите пятизначное положительное число");
 string number = Console.ReadLine();
 char[] array = number.ToCharArray();
-if (array[0] == array[4] | array[1] == array[3])
+if (array[0] == array[4] && array[1] == array[3])
 Console.WriteLine("палиндром");
 else
 Console.WriteLine("не палиндром");
