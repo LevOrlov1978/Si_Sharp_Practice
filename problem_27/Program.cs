156ed0c51d6404b9534f218fad8c55bf362f03e1
email <or_lev@mail.ru>
2022-08-17 20:14:25 +0300

    задача 27

---
+++ problem_27/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите целое положительное число");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int NumbersSum (int Number)
+{
+int Sum=0;
+while (Number>0)
+{
+
+Sum=Sum+Number%10;
+Number=Number/10;
+}
+return Sum;
+}
+Console.WriteLine($"Сумма цифр {NumbersSum(N)}");
