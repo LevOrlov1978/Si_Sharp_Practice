e93a36fe75298fad52fa72279a0120693ffcd51d
email <or_lev@mail.ru>
2022-08-17 19:33:52 +0300

    задача 25

---
+++ problem_25/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите основание степени");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите показатель степени");
+int B = Convert.ToInt32(Console.ReadLine());
+int Sum=1;
+for (int i = 0; i < B; i++)
+{
+    Sum=Sum*A;
+}
+Console.WriteLine($"A в степени B = {Sum}");
