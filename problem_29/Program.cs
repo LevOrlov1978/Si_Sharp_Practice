ab0ffac704574af7f55b075ab89954866dcbbffb
email <or_lev@mail.ru>
2022-08-17 21:18:48 +0300

    задача 29

---
+++ problem_29/Program.cs
@@ -0,0 +1,8 @@
+﻿int[] array = new int[8];
+
+for (int i = 0; i <= 7; i++)
+{
+Console.WriteLine ($"Введите элемент массива №{i+1}"); 
+array[i] = Convert.ToInt32(Console.ReadLine()); 
+}
+Console.WriteLine($"Массив: [{String.Join(";",array)}]");
