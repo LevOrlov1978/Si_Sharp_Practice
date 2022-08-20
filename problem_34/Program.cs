b72129059066c022afb4cf8b5b33db9a0ff7b8d3
email <or_lev@mail.ru>
2022-08-20 21:59:27 +0300

    задача 34

---
+++ problem_34/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите количество элементов массива");
+int ArrayLength = Convert.ToInt32(Console.ReadLine());
+
+int EvenSum (int Length)
+{
+int Count=0;
+int[] array = new int[Length];
+
+for (int i = 0; i < Length; i++)
+{
+    array[i] = new Random().Next(100,1000);  
+    if (array[i]%2==0)
+    {
+        Count=Count+1;
+    }
+}
+Console.WriteLine($"Массив: [{String.Join(";",array)}]");
+return Count;
+}
+Console.WriteLine($"Количество четных {EvenSum(ArrayLength)}");
