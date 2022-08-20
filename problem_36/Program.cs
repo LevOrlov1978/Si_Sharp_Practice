fb2b0cf5e66b5a6f74808d437c4e3f9d3247ca30
email <or_lev@mail.ru>
2022-08-20 22:23:55 +0300

    задача 36

---
+++ problem_36/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Введите количество элементов массива");
+int ArrayLength = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите X (границы случайных  значений от -Х до +Х)");
+int RandomBorder = Convert.ToInt32(Console.ReadLine());
+
+int NotEvenSum (int Length, int Border)
+{
+int Sum=0;
+int[] array = new int[Length];
+
+for (int i = 0; i < Length; i++)
+{
+    array[i] = new Random().Next((-1*Border-1),(Border+1));  
+    if (i%2!=0)
+    {
+       Sum=Sum+array[i];
+    }
+}
+Console.WriteLine($"Массив: [{String.Join(";",array)}]");
+return Sum;
+}
+Console.WriteLine($"Сумма на нечетных позициях: {NotEvenSum(ArrayLength, RandomBorder)}");
