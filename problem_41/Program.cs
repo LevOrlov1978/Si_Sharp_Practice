bf4365ec55b2e1a34a4c9f669a5403a10c18b1e7
email <or_lev@mail.ru>
2022-08-26 22:20:33 +0300

    Задача 41

---
+++ problem_41/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите необходимое количество чисел");
+int M = Convert.ToInt32(Console.ReadLine());
+int countPositive = 0;
+ 
+for (int i = 1; i <= M; i++)
+{
+    Console.WriteLine($"Введите число {i}");
+    double temporaryNumber = Convert.ToDouble(Console.ReadLine());
+   
+    if (temporaryNumber>0)
+    {
+        countPositive=countPositive+1;  
+    }
+}
+Console.WriteLine($"Чисел больше нуля -  {countPositive}");
