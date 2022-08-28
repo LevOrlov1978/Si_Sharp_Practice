53bf7bf13ee4326ca986c5e221885bbc08b34121
email <or_lev@mail.ru>
2022-08-28 22:36:24 +0300

    Задача 50

---
+++ problem_50/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int [,] matrix = new int[rows,columns];
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        matrix[i,j]  = new Random().Next(-11,11);
+
+        Console.Write($" | {matrix[i,j]}");
+    }
+    Console.WriteLine();
+}
+Console.Write("Введите номер строки элемента:");
+int rowNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите номер столбца элемента: ");
+int columnNumber = Convert.ToInt32(Console.ReadLine());
+
+if (rowNumber<=rows && columnNumber<=columns)
+{
+    Console.WriteLine ($"Элемент массива:{matrix[rowNumber-1,columnNumber-1]}");   
+}
+else
+{
+    Console.WriteLine("Такого элемента нет");
+}
