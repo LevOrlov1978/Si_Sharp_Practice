0fe8c27cfca69bb52f6e00ffb446498c1ca093a8
email <or_lev@mail.ru>
2022-08-28 22:21:41 +0300

    Задача 52

---
+++ problem_47/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+double [,] matrix = new double[rows,columns];
+
+//массив с случайными вещественными числами от -10 до 10
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        matrix[i,j]  = Math.Round(new Random().Next(-11,11)*
+                                new Random().NextDouble(),2);
+
+        Console.Write($" | {matrix[i,j]}");
+    }
+    Console.WriteLine();
+}
---
+++ problem_52/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Write("Введите количество строк: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int [,] matrix = new int[rows,columns];
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        matrix[i,j]  = new Random().Next(-11,11);
+
+        Console.Write($" | {matrix[i,j]}");
+    }
+    Console.WriteLine();
+    
+}
+Console.WriteLine();
+Console.WriteLine("Среднее арифметическое по столбцам:");
+for (int j = 0; j < columns; j++)
+{
+    double tempSum=0; //сумма столбца, обнуляется после подсчета и вывода среднего
+
+    for (int i = 0; i < rows; i++)
+   
+    {
+        tempSum=tempSum+matrix[i,j];
+
+    }
+   double average = Math.Round(tempSum/rows,2);
+   Console.Write($" | {average}");
+}
+Console.WriteLine();
