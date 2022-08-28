6138b8e2c3674f66fbc07cc293974af4ee236f9f
email <or_lev@mail.ru>
2022-08-28 18:50:27 +0300

    десятичное  в двоичное

---
+++ dec _to_bin/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число в десятичной системе");
+int number = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[number];
+int count=0;
+while (number != 0)
+{ 
+    array[count] = number%2;
+    count=count+1;
+    number=number/2; 
+
+}
+
+
+for (int i = count-1; i>-1; i--)
+{
+    Console.Write(array[i]); 
+}
+Console.WriteLine();
