49b3efe7f2080a077c8c1193af819001122ca876
email <or_lev@mail.ru>
2022-09-07 20:23:12 +0300

    Задача 66

---
+++ problem_66/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите целое неотрицательное число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите целое неотрицательное число n, n>m: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int Sum (int number1, int number2)
+{
+if (number2-number1 == 1) return number1+number2;
+
+ return (number1+Sum(number1+1, number2));
+
+ 
+ }
+Console.WriteLine(Sum(m,n));
